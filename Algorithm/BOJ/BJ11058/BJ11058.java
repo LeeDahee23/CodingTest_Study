449afd1e9b1eb49aad93f11fe5fc4c2a79297b9c
Dahee Lee <82389864+LeeDahee23@users.noreply.github.com>
2023-10-25 12:10:48 +0900

    백준 11058 크리보드

---
+++ Algorithm/BOJ/BJ11058/BJ11058.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class BJ11058 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		long[] dp = new long[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			if(dp[i] <= 0) {
+        dp[i] = i;
+      }
+			else {
+				dp[i] = Math.max(dp[i], i);
+			}
+			for(int j=i+3; j<=N; j++) {
+				dp[j] = Math.max(dp[j], dp[i] * (j-i-1)); 
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
+
+}
