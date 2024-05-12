54439d4a1495493cef089a835614453cad765400
Dahee Lee <dduosy9@gmail.com>
2024-05-12 22:48:31 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+class Word{
+    String word;
+    int cnt;
+    
+    Word(String word, int cnt){
+        this.word = word;
+        this.cnt = cnt;
+    }
+}
+
+class Solution {
+    private int answer = 0;
+    public int solution(String begin, String target, String[] words) {
+        // target이 words에 없으면 return 0
+        boolean hasTarget = false;
+        for(String word : words){
+            if(word.equals(target)){
+                hasTarget = true;
+                break;
+            }
+        }
+        
+        if(!hasTarget) return 0;
+        
+        find(words, begin, target);
+        return answer;
+    }
+    
+    private void find(String[] words, String start, String target){
+        Queue<Word> q = new LinkedList<>();
+        boolean[] visited = new boolean[words.length];
+        q.add(new Word(start, 0));
+        
+        while(!q.isEmpty()){
+            Word now = q.poll();
+            
+            if(target.equals(now.word)){
+                answer = now.cnt;
+                break;
+            }
+            
+            for(int i=0; i<words.length; i++){
+                if(!visited[i] && isDiffOnlyOne(now.word, words[i])){
+                    visited[i] = true;
+                    q.add(new Word(words[i], now.cnt + 1));
+                    
+                }
+            }
+        }
+        
+    }
+    
+    private boolean isDiffOnlyOne(String word1, String word2){
+        int cnt = 0;
+        for(int i=0; i<word1.length(); i++){
+            if(word1.charAt(i) != word2.charAt(i)){
+                cnt ++;
+            }
+        }
+        
+        return cnt == 1 ? true : false;
+    }
+}
