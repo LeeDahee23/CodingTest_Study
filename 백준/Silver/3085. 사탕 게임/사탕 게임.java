30ddcc817f58ceb29ab437025068d2304362be76
Dahee Lee <dduosy9@gmail.com>
2024-05-14 15:57:29 +0900

    [Silver II] Title: 사탕 게임, Time: 128 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -5,11 +5,12 @@ public class Main {
 	private static final int[] dx = {0, 0, 1, -1};
 	private static final int[] dy = {1, -1, 0, 0};
 	private static int max = 0;
+	private static char[][] board;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		char[][] board = new char[N][N];
+		board = new char[N][N];
 		
 		for(int i=0; i<N; i++) {
 			String s = br.readLine();
@@ -33,55 +34,57 @@ public class Main {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			
-			char[][] tmpBoard = new char[board.length][board[0].length];
-			for(int j=0; j<board.length; j++) {
-				tmpBoard[j] = board[j].clone();
-			}
-			
 			// 인접한 사탕이 범위 안에 있고, 나의 색과 다른 사탕이라면
-			if(nx>=0 && nx<board.length && ny>=0 && ny<board.length && tmpBoard[x][y] != tmpBoard[nx][ny]) {
+			if(nx>=0 && nx<board.length && ny>=0 && ny<board.length) {
 				// 나랑 인접한 사탕을 교환함
-				char tmp = tmpBoard[x][y];
-				tmpBoard[x][y] = tmpBoard[nx][ny];
-				tmpBoard[nx][ny] = tmp;
-				
+				swap(x, y, nx, ny);				
 				// 교환했을 때 가장 긴 연속 부분의 길이를 구함
-				int cnt = findLongestLength(tmpBoard);
-				max = Math.max(max, cnt);
+				findLongestLength(x, y);
+				swap(x, y, nx, ny);
 			}
 		}
 	}
 	
-	private static int findLongestLength(char[][] board) {
-		int cnt, max = 0;
-		// 행의 길이
-		for(int i=0; i<board.length; i++) {
-			cnt = 1;
-			for(int j=0; j<board[i].length-1; j++) {
-				if(board[i][j] == board[i][j+1]) cnt++;
-				else {
-					max = Math.max(max, cnt);
-					cnt = 1;
-				}
-			}
-			
-			max = Math.max(max, cnt);
+	private static void swap(int x, int y, int nx, int ny) {
+		char tmp = board[x][y];
+		board[x][y] = board[nx][ny];
+		board[nx][ny] = tmp;
+	}
+	
+	// 문제점
+	// 중복된 연산이 너무 많음(현재 위치의 행열만 살펴보면 됨!)
+	private static void findLongestLength(int x, int y) {
+		int cnt = 1;
+		// 행의 길이: 열은 고정, 행만 위,아래를 나와 같은지 살펴보면 됨
+		int zx = x-1;
+		while(true) {
+			if(zx<0 || board[x][y] != board[zx--][y]) break;
+			cnt ++;
 		}
+		
+		zx = x+1;
+		while(true) {
+			if(zx>=board.length || board[x][y] != board[zx++][y]) break;
+			cnt ++;
+		}
+		
+		max = Math.max(max, cnt);
 		
 		// 열의 길이
-		for(int i=0; i<board.length; i++) {
-			cnt = 1;
-			for(int j=0; j<board[i].length-1; j++) {
-				if(board[j][i] == board[j+1][i]) cnt++;
-				else {
-					max = Math.max(max, cnt);
-					cnt = 1;
-				}
-			}
-			
-			max = Math.max(max, cnt);
+		cnt = 1;
+		
+		int zy = y-1;
+		while(true) {
+			if(zy<0 || board[x][y] != board[x][zy--]) break;
+			cnt ++;
 		}
 		
-		return max;
+		zy = y+1;
+		while(true) {
+			if(zy>=board[0].length || board[x][y] != board[x][zy++]) break;
+			cnt ++;
+		}
+		
+		max = Math.max(max, cnt);
 	}
 }
