a397ac6a21c176b0c3667db989bc26fd2cb31231
Dahee Lee <dduosy9@gmail.com>
2024-05-14 10:22:40 +0900

    [level 2] Title: 방문 길이, Time: 15.89 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,31 +1,22 @@
 import java.util.*;
-
-class Dir{
-    int x, y;
-    Dir(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-
 class Solution {
     private int[][] board = new int[11][11];
     public int solution(String dirs) {
-        HashMap<Character, Dir> map = new HashMap<>();
-        map.put('U', new Dir(-1, 0));
-        map.put('D', new Dir(1, 0));
-        map.put('L', new Dir(0, -1));
-        map.put('R', new Dir(0, 1));
+        HashMap<Character, int[]> map = new HashMap<>();
+        map.put('U', new int[]{-1, 0});
+        map.put('D', new int[]{1, 0});
+        map.put('L', new int[]{0, -1});
+        map.put('R', new int[]{0, 1});
         
         HashSet<String> set = new HashSet<>();
         
         int x = 5, y = 5;
         for(int i=0; i<dirs.length(); i++){
             char c = dirs.charAt(i);
-            Dir dir = map.get(c);
+            int[] dir = map.get(c);
             
-            int nx = x + dir.x;
-            int ny = y + dir.y;
+            int nx = x + dir[0];
+            int ny = y + dir[1];
             
             if(nx>=0 && nx<board.length && ny>=0 && ny<board.length){
                 set.add(x + " " + y + " " + nx + " " + ny);
