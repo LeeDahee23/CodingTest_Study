a6069d2460abf03d3e016fac15c911c938f9dc57
Dahee Lee <dduosy9@gmail.com>
2024-06-04 10:39:28 +0900

    [level 2] Title: 숫자 변환하기, Time: 26.29 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,22 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-        int[] dp = new int[1000001];
+        int[] dp = new int[3000003];
         Arrays.fill(dp, -1);
         dp[x] = 0;
-        
+
         for(int i=x; i<=Math.max(y/3, y-n); i++){
             if(dp[y] > 0) break;
             if(dp[i] < 0) continue;
-    
-            if(i+n > dp.length) continue;
+
             dp[i+n] = dp[i+n] < 0 ? dp[i] + 1 : Math.min(dp[i+n], dp[i] + 1);
-            if(i*2 > dp.length) continue;
             dp[i*2] = dp[i*2] < 0 ? dp[i] + 1 : Math.min(dp[i*2], dp[i] + 1);
-            if(i*3 > dp.length) continue;
             dp[i*3] = dp[i*3] < 0 ? dp[i] + 1 : Math.min(dp[i*3], dp[i] + 1);
         }
-        
+
         return dp[y];
     }
 }
