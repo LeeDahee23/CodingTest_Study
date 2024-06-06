070a059c989160b4bb51fe14e9006dc66edcc604
Dahee Lee <dduosy9@gmail.com>
2024-06-06 10:22:25 +0900

    [level 3] Title: 숫자 게임, Time: 56.81 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -4,20 +4,13 @@ class Solution {
     public int solution(int[] A, int[] B) {
         Arrays.sort(A);
         Arrays.sort(B);
-        Queue<Integer> que = new LinkedList<>();
-        for(int i=0; i<B.length; i++){
-            que.offer(B[i]);
-        }
         
         int answer = 0;
-        for(int i=0; i<A.length;){
-            if(que.isEmpty()) break;
-            
-            if(A[i] < que.peek()){
-                i++;
+        for(int i=0, j=0; j<B.length; j++){
+            if(A[i] < B[j]){
                 answer ++;
+                i ++;
             }
-             que.poll();
         }
             
         return answer;
